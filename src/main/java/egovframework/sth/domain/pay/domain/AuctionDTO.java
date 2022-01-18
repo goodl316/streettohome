b5d4ca83d19f3ac1b408335f026e6913dba48793
JINCHEOL KIM <jincheol2578@gmail.com>
2022-01-18 17:50:12 +0900

    22.01.18

---
+++ src/main/java/egovframework/sth/domain/pay/domain/AuctionDTO.java
@@ -0,0 +1,49 @@
+package egovframework.sth.domain.pay.domain;
+
+public class AuctionDTO {
+	
+	private int b_no;
+	private int m_no;
+	private int ac_cur_winner;
+	private int ac_price;
+	private int ac_startprice;
+	private String ac_enddt;
+	public int getB_no() {
+		return b_no;
+	}
+	public void setB_no(int b_no) {
+		this.b_no = b_no;
+	}
+	public int getM_no() {
+		return m_no;
+	}
+	public void setM_no(int m_no) {
+		this.m_no = m_no;
+	}
+	public int getAc_cur_winner() {
+		return ac_cur_winner;
+	}
+	public void setAc_cur_winner(int ac_cur_winner) {
+		this.ac_cur_winner = ac_cur_winner;
+	}
+	public int getAc_price() {
+		return ac_price;
+	}
+	public void setAc_price(int ac_price) {
+		this.ac_price = ac_price;
+	}
+	public int getAc_startprice() {
+		return ac_startprice;
+	}
+	public void setAc_startprice(int ac_startprice) {
+		this.ac_startprice = ac_startprice;
+	}
+	public String getAc_enddt() {
+		return ac_enddt;
+	}
+	public void setAc_enddt(String ac_enddt) {
+		this.ac_enddt = ac_enddt;
+	}
+	
+	
+}
