484e5c695bcdf1c2ae587daf56e9050a50fc3451
JINCHEOL KIM <jincheol2578@gmail.com>
2022-01-25 09:24:54 +0900

    22.01.25

--- src/main/java/egovframework/sth/domain/board/mapper/BoardMapper.java
+++ src/main/java/egovframework/sth/domain/board/mapper/BoardMapper.java
@@ -5,24 +5,23 @@ import java.util.List;
 import org.egovframe.rte.psl.dataaccess.mapper.Mapper;
 
 import egovframework.sth.domain.board.domain.AnimalDTO;
+import egovframework.sth.domain.board.domain.AuctionDTO;
 import egovframework.sth.domain.board.domain.BoardDTO;
 import egovframework.sth.domain.board.domain.BoardVO;
 import egovframework.sth.domain.board.domain.BoardViewVO;
-import egovframework.sth.domain.pay.domain.AuctionDTO;
 
 @Mapper
 public interface BoardMapper {
 	
 	BoardViewVO selBoardView(BoardDTO param);
-    public List<BoardVO> boardList(BoardVO vo);
-    int insBoard(BoardDTO dto);
-    int insAnimal(AnimalDTO dto);
-    int updpatImg(AnimalDTO dto);
-    BoardVO countBoard(BoardVO vo);
-    AnimalDTO selinfo(AnimalDTO dto);
-    BoardVO modselboard(BoardVO vo);
-    int updateBoard(BoardVO vo);
-    int updateAnimal(BoardVO vo);
-    int insAuction(AuctionDTO dto);
-    int boardHit(int b_no);
+	public List<BoardVO> boardList(BoardVO vo);
+	int insBoard(BoardDTO dto);
+	int insAnimal(AnimalDTO dto);
+	int updpatImg(AnimalDTO dto);
+	BoardVO countBoard(BoardVO vo);
+	AnimalDTO selinfo(AnimalDTO dto);
+	BoardVO modselboard(BoardVO vo);
+	int updateBoard(BoardVO vo);
+	int updateAnimal(BoardVO vo);
+	int insAuction(AuctionDTO dto);
 }
